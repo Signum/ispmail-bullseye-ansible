6b1e03d554854a29fde3d7d78beeef28b5e85a4a
Christoph Haas <email@christoph-haas.de>
2015-10-04 20:21:25 +0200

    Inserting test data into database

---
+++ roles/ispmail-database/files/test.sql
@@ -0,0 +1,26 @@
+REPLACE INTO `mailserver`.`virtual_domains` (
+      `id` ,
+      `name`
+)
+VALUES (
+      '1', 'example.org'
+);
+REPLACE INTO `mailserver`.`virtual_users` (
+      `id` ,
+      `domain_id` ,
+      `password` ,
+      `email`
+)
+VALUES (
+      '1', '1', MD5( 'summersun' ) , 'john@example.org'
+);
+REPLACE INTO `mailserver`.`virtual_aliases` (
+      `id`,
+      `domain_id`,
+      `source`,
+      `destination`
+)
+VALUES (
+      '1', '1', 'jack@example.org', 'john@example.org'
+);
+
